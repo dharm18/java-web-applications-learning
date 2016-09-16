2208da963feeee7c9a50b60a7962dbf3ec42ad5c
Dharmendra <vishdharm.181910+github@gmail.com>
2016-09-16 18:16:13 +0530

    - commit leftover file.

---
+++ test/src/com/test/calculator/IArithmeticCalculator.java
@@ -0,0 +1,9 @@
+package com.test.calculator;
+
+public interface IArithmeticCalculator {
+
+	public double add(double a, double b);
+	public double sub(double a, double b);
+	public double mul(double a, double b);
+	public double div(double a, double b);
+}
